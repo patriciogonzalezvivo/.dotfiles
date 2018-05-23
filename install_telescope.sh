96dec18166bcab46f20d98fe6527c20aff202017
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2018-05-23 05:35:02 -0700

    telescope all the way

--- install_telescope.sh
+++ install_telescope.sh
@@ -31,22 +31,22 @@ if [ $os == "Linux" ]; then
     if [ $arq == "armv6l" ] || [ $arq == "armv7l" ]; then
         sudo apt-get install $apps_linux_rpi
 
-        mkdir /opt/libindi
+        sudo mkdir /opt/libindi
         pushd /opt/libindi
-          curl -o libindi_rpi.tgz -L http://indilib.org/download/raspberry-pi/send/6-raspberry-pi/9-indi-library-for-raspberry-pi.html
-          tar -xzf libindi_rpi.tgz --strip 1
+          sudo curl -o libindi_rpi.tgz -L http://indilib.org/download/raspberry-pi/send/6-raspberry-pi/9-indi-library-for-raspberry-pi.html
+          sudo tar -xzf libindi_rpi.tgz --strip 1
  
-          dpkg -i *.deb
+          sudo dpkg -i *.deb
           sudo apt-get -f install 
         popd
 
-        git clone http://github.com/knro/indiwebmanager /opt/indiwebmanager
-        cp /opt/indiwebmanager/indiwebmanager.service /lib/systemd/system/
+        sudo git clone http://github.com/knro/indiwebmanager /opt/indiwebmanager
+        sudo cp /opt/indiwebmanager/indiwebmanager.service /lib/systemd/system/
 
-        sed -i 's|/home/patricio|/opt/indiwebmanager|g' /lib/systemd/system/indiwebmanager.service
-        sed -i 's/User=patricio/User=root/g' /lib/systemd/system/indiwebmanager.service
-        chmod 644 /lib/systemd/system/indiwebmanager.service
-        ln -s /lib/systemd/system/indiwebmanager.service /etc/systemd/system/multi-user.target.wants/indiwebmanager.service
+        sudo sed -i 's|/home/patricio|/opt/indiwebmanager|g' /lib/systemd/system/indiwebmanager.service
+        sudo sed -i 's/User=patricio/User=root/g' /lib/systemd/system/indiwebmanager.service
+        sudo chmod 644 /lib/systemd/system/indiwebmanager.service
+        sudo ln -s /lib/systemd/system/indiwebmanager.service /etc/systemd/system/multi-user.target.wants/indiwebmanager.service
 
         echo "INDI web admin running on localhost:8624"
 
