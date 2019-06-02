7e987d9ab1494bc1f379029d2f62a22f81c1f530
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2019-06-02 09:37:53 -0700

    fixing dependencies

--- install_python.sh
+++ install_python.sh
@@ -5,7 +5,7 @@ arq=$(uname -m)
 
 apps_osx="python freetype pyqt sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer"
 apps_linux_rpi="python-dev libxml2-dev libxslt-dev python3-lxml python-lxml libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libgl1-mesa-dev libgles2-mesa-dev python-setuptools libgstreamer1.0-dev gstreamer1.0-plugins-{bad,base,good,ugly} gstreamer1.0-{omx,alsa} libmtdev-dev xclip xsel"
-apps_linux_ubuntu="python2-dev python2-pip python3-dev python3-pip ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good"
+apps_linux_ubuntu="python-dev python-pip python3-dev python3-pip ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good"
 python_global="virtualenv virtualenvwrapper "
 python_base_modules="turses numpy scipy matplotlib jupyter jupyterlab"
 python_ui_modules="Cython==0.28.2 kivy "
