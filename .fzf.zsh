a0fc4d26a2ae1b62fe931150aae91d9450639e23
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-09-24 12:42:47 -0400

    kitty theme

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/patricio/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/patricio/.fzf/bin"
+if [[ ! "$PATH" == */home/patricio/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/patricio/.fzf/bin"
 fi
 
 source <(fzf --zsh)
